ce3db049394e2979ab8f19d72958390d747ac9cc
Chris-moffitt <moffitt.a.chris@gmail.com>
2016-01-19 18:49:13 -0500

    Update JSONtoMongo.js

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -7,16 +7,23 @@ var fs = require('fs'),
     mongoose = require('mongoose'), 
     Schema = mongoose.Schema, 
     Listing = require('./ListingSchema.js'), 
-    config = require('./config');
+    config = require('./config.js'),
+    listingData= require('./Listings.json');
 
 /* Connect to your database */
-
+mongoose.connect(config.db.uri);
 /* 
   Instantiate a mongoose model for each listing object in the JSON file, 
   and then save it to your Mongo database 
  */
-
-
+for(var i = 0; i<listingData.entries.length; i++){
+	var entry=listingData.entries[i];
+	var newEntry=Listing(entry);
+	newEntry.save(function(err) {
+		if (err) throw err;
+	});
+}
+console.log('Success!');
 /* 
   Once you've written + run the script, check out your MongoLab database to ensure that 
   it saved everything correctly. 
